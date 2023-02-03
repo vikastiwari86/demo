01a8cb49b6ccf9896b6b80c831df12e2f1ba326a
vikastiwari86 <80738713+vikastiwari86@users.noreply.github.com>
2023-02-03 09:37:55 +0530

    Delete src/main directory

--- src/main/java/com/zensar/ankit/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.ankit.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zensar.ankit.demo.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- src/main/java/com/zensar/ankit/demo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.zensar.ankit.demo.service;
-
-import com.zensar.ankit.demo.entity.User;
-
-public interface UserService {
-	public User save(User user);
-}
